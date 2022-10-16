988e0eef35080ace07a78fdab20030e8c8314412
muhammad <msmainacc0unt@gmail.com>
2022-10-16 08:50:07 +0200

    fix cached screen height

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@ import useChatStore from './stores/chat';
 import FadeTransition from './components/reusables/transitions/FadeTransition.vue'; const chat = useChatStore();
 
 
-const vh = computed(() => {
+const vh = () => {
     return `${window.innerHeight}px`;
-});
+};
 </script>
 
 <template>
     <div :class="{'dark': chat.settings[2].settings[0].value}">
-        <div class="bg-white dark:bg-gray-800 transition-all duration-500" :style="{height: vh}">
+        <div class="bg-white dark:bg-gray-800 transition-all duration-500" :style="{height: vh()}">
             <router-view v-slot="{ Component }">
                 <FadeTransition>
                     <component :is="Component" />
