df86425a8c2c63bf41514fe8474a5db478e7fc50
Demon Archon <42561778+demon-bixia@users.noreply.github.com>
2022-10-31 12:27:26 -0700

    make app height resizes when the window resizes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { ref, computed, onMounted, onUnmounted } from "vue";
 
 import useChatStore from './stores/chat';
 
 import FadeTransition from './components/reusables/transitions/FadeTransition.vue'; const chat = useChatStore();
 
+// todo add video calling
+// todo refactor tailwind classes
 
-const vh = () => {
-    return `${window.innerHeight}px`;
+// the app height
+const height = ref(`${window.innerHeight}px`);
+
+// change the app height whenever the window resizes
+const resizeWindow = () => {
+    height.value = `${window.innerHeight}px`;
 };
+
+// add the resize event when the component mounts
+onMounted(() => {
+    window.addEventListener('resize', resizeWindow);
+});
+
+// remove the event when un-mounting the component
+onUnmounted(() => {
+    window.removeEventListener('resize', resizeWindow);
+});
 </script>
 
 <template>
-    <div :class="{'dark': chat.settings[2].settings[0].value}">
-        <div class="bg-white dark:bg-gray-800 transition-all duration-500" :style="{height: vh()}">
+    <div :class="{ 'dark': chat.settings[2].settings[0].value }">
+        <div class="bg-white dark:bg-gray-800 transition-all duration-500" :style="{ height: height }">
             <router-view v-slot="{ Component }">
                 <FadeTransition>
                     <component :is="Component" />
