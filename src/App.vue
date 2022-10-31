da1f23f5b19244fc2bc8d39eb3b9885fa85174b8
Demon Archon <42561778+demon-bixia@users.noreply.github.com>
2022-10-31 12:27:26 -0700

    add todos

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@ import useChatStore from './stores/chat';
 
 import FadeTransition from './components/reusables/transitions/FadeTransition.vue'; const chat = useChatStore();
 
+// fix height on mobile screens
+// todo add video calling
+// todo refactor tailwind classes
+
 
 const vh = () => {
     return `${window.innerHeight}px`;
@@ -12,8 +16,8 @@ const vh = () => {
 </script>
 
 <template>
-    <div :class="{'dark': chat.settings[2].settings[0].value}">
-        <div class="bg-white dark:bg-gray-800 transition-all duration-500" :style="{height: vh()}">
+    <div :class="{ 'dark': chat.settings[2].settings[0].value }">
+        <div class="bg-white dark:bg-gray-800 transition-all duration-500" :style="{ height: vh() }">
             <router-view v-slot="{ Component }">
                 <FadeTransition>
                     <component :is="Component" />
