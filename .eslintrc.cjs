f24cf2cb588d027a8c288afd796868a42c4a75a0
mahmoud <info@webafrooz.com>
2023-09-10 07:14:52 +0330

    Add linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-unused-vars": "off",
+    "vue/multi-word-component-names": "off",
+  },
+};
